16977aeb2ac6aef1b87c1ce03c43bfc3528c928c
Jason Gray <jgray7693@gmail.com>
2023-12-02 18:11:37 -0800

    Initial Commit Day 2

---
+++ day2/Cube.cpp
@@ -0,0 +1,21 @@
+//Jason Gray
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    const int blue_max = 12;
+    const int green_max = 13;
+    const int red_max = 14;
+
+    ifstream fin;
+    fin.open("Input.txt");
+
+    string input;
+
+    while(getline(fin, input)){
+        
+    }
+}
